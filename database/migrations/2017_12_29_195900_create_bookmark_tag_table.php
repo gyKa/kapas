34faa1457ce146b85d41cf8d47e804c2069d50b9
Gytis Karčiauskas <gytis@karciauskas.lt>
2017-12-29 22:01:55 +0200

    Create ManyToMany table for bookmarks and tags.

---
+++ database/migrations/2017_12_29_195900_create_bookmark_tag_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookmarkTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookmark_tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('bookmark_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
+
+            $table->unique(['bookmark_id', 'tag_id']);
+
+            $table->foreign('bookmark_id')
+                ->references('id')->on('bookmarks')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreign('tag_id')
+                ->references('id')->on('tags')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookmark_tag');
+    }
+}
