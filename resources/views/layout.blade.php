f436a7982358ea479564103bde1f731604f7dddc
Gytis Karčiauskas <gytis@karciauskas.lt>
2017-12-30 11:11:09 +0200

    Use Bulma CSS framework.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>kapas</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
+    </head>
     <body>
-        @yield('content')
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
