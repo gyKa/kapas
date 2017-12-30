23bf6a58af1dbfb924c7c928e46ea996c0b0cb78
Gytis Karčiauskas <gytis@karciauskas.lt>
2017-12-30 11:33:33 +0200

    Use Bulma theme.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>kapas</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
+        <link rel="stylesheet" href="https://unpkg.com/bulmaswatch/darkly/bulmaswatch.min.css">
     </head>
     <body>
         <div class="container">
